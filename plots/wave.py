283c3d76717160c3fc5c00a403c9434d0aee0794
rmflynn <rmfofco@gmail.com>
2020-03-27 11:44:43 -0600

    Add the code to make plots

---
+++ plots/wave.py
@@ -0,0 +1,24 @@
+# Import the scripts
+import pandas as pd
+import seaborn as sea
+import matplotlib.pyplot as plt
+
+data = pd.read_feather("../flat_data/Staging_Data_GT_190315.ftr")
+
+#sea.lineplot(y='eeg', x='index', data=data[:500])
+#plt.show()
+
+eeg = data[['eeg']][:1000]
+eog = data[['eog']][:1000]
+emg = data[['emg']][:1000]
+eeg.columns = ['measure']
+eog.columns = ['measure'] 
+emg.columns = ['measure']
+eeg['wave'] = 'eeg'
+eog['wave'] = 'eog'
+emg['wave'] = 'emg'
+data_joined = pd.concat([eeg, eog, emg], axis=0)
+data_joined['index'] = data_joined.index
+plot = sea.lineplot(y='measure', x='index', hue='wave', data=data_joined)
+plot.figure.savefig("waveplot.png")
+
