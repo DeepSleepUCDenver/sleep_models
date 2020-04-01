6260cbee42e983a4f784f1ea0482c67a08ac5304
Tom <Thomas.Bekman12@ncf.edu>
2020-03-31 21:41:25 -0400

    AutoEncoder
    Early coded up attempt at an autoencoder for raw time-series data. 
    
    Goal: Have some concrete results to show performance of the model, and start tuning.
    Issues: Does Binary cross entropy really make sense for reconstruction loss? The # of parameters are really small for this model, its probably not enough to get it working well.

---
+++ AutoEncoder.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+import keras
+from keras.layers import Input, Dense, Conv1D, MaxPooling1D, UpSampling1D, BatchNormalization, LSTM, RepeatVector
+from keras.models import Model
+from keras.models import model_from_json
+from keras import regularizers
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn import preprocessing
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib
+
+
+
+
+data = pd.read_feather("Staging_Data_GT_190315.ftr")
+
+
+eeg = data[['eeg']].to_numpy().transpose()
+eog = data[['eog']].to_numpy().transpose()
+emg = data[['emg']].to_numpy().transpose()
+
+data = np.vstack((eeg,eog,emg))
+
+window_length = data.shape[1]
+
+
+#TODO: Normalize Data
+
+#Encoder
+input_window = Input(shape=(window_length,3))
+x = Conv1D(16, 3, activation="relu", padding="same")(input_window) # Full Dimension
+#x = BatchNormalization()(x)
+x = MaxPooling1D(2, padding="same")(x)
+x = Conv1D(1, 3, activation="relu", padding="same")(x)
+#x = BatchNormalization()(x)
+encoded = MaxPooling1D(2, padding="same")(x) # 3 dims... I'm not super convinced this is actually 3 dimensions
+
+encoder = Model(input_window, encoded)
+
+# 3 dimensions in the encoded layer
+
+x = Conv1D(1, 3, activation="relu", padding="same")(encoded) # Latent space
+#x = BatchNormalization()(x)
+x = UpSampling1D(2)(x) # 6 dims
+x = Conv1D(16, 2, activation='relu')(x) # 5 dims
+#x = BatchNormalization()(x)
+x = UpSampling1D(2)(x) # 10 dims
+decoded = Conv1D(1, 3, activation='sigmoid', padding='same')(x) # 10 dims
+autoencoder = Model(input_window, decoded)
+autoencoder.summary()
+
+
+x_train = data
+epochs = 2
+
+
+
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+history = autoencoder.fit(x_train, x_train,
+                epochs=epochs,
+                batch_size=1024,
+                shuffle=True,
+                validation_data=(x_train, x_train))
+
+#decoded_stocks = autoencoder.predict(x_test)
+
+
+#Source
+#https://towardsdatascience.com/autoencoders-for-the-compression-of-stock-market-data-28e8c1a2da3e
+#Look there for some good graphing code!
